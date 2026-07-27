13dd544dd436b48596af3099e39d94becfc4bff5
Kingron <kiruiaron783@gmail.com>
2026-07-27 16:20:02 +0300

    Update connect button functionality for OAuth

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const connectBtn = document.getElementById("connect");
+const APP_ID = "33WKSW7cKl67wCLdJFnSk";
+const REDIRECT_URI = "https://kingron10.github.io/kingron10/";
 
-connectBtn.addEventListener("click", () => {
-    window.open("https://deriv.com", "_self");
+document.getElementById("connect").addEventListener("click", () => {
+    window.location.href = `https://oauth.deriv.com/oauth2/authorize?app_id=${APP_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
 });
